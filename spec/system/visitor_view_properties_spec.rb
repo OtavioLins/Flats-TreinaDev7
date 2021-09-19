3024d6e450f7287d7f20277f298dcdfd8325a807
Otávio Lins <otavioslins97@gmail.com>
2021-09-19 16:33:19 -0300

    Visitor view property details

--- spec/system/visitor_view_properties_spec.rb
+++ spec/system/visitor_view_properties_spec.rb
@@ -5,11 +5,13 @@ describe 'Visitor visits homepage' do
         #Arrange (Preparar os Dados)
         Property.create({title: "Casa em Copacabana", 
                          description: "Excelente casa, recém reformada com 2 
-                         vagas de garagem", rooms: 3})
+                         vagas de garagem", rooms: 3, parking_slot: true, bathrooms: 2,
+                         pets: true, daily_rate: 500})
 
         Property.create({title: "Cobertura em Manaus", 
                          description: "Cobertura de 300m2, churrasqueira e 
-                         sauna privativa", rooms: 5})
+                         sauna privativa", rooms: 5, parking_slot: false, bathrooms: 3,
+                         pets: false, daily_rate: 750})
 
         #Act (Executar a funcionalidade)
         visit root_path
@@ -22,5 +24,28 @@ describe 'Visitor visits homepage' do
         expect(page).to have_text("Cobertura em Manaus")
         expect(page).to have_text("Cobertura de 300m2, churrasqueira e sauna privativa")
         expect(page).to have_text("Quartos: 5")
+    end
+
+    it 'and there are no properties available' do
+        visit root_path
+        expect(page).to have_text("Nenhum imóvel disponível")
+    end
+    
+    it 'and view property details' do
+        Property.create({title: "Casa em Copacabana", 
+                        description: "Excelente casa, recém reformada com 2 
+                        vagas de garagem", rooms: 3, parking_slot: true, bathrooms: 2,
+                        pets: true, daily_rate: 500})
+        
+        visit root_path
+
+        click_on "Casa em Copacabana"
+
+        expect(page).to have_text("Casa em Copacabana")
+        expect(page).to have_text("Excelente casa, recém reformada com 2 vagas de garagem")
+        expect(page).to have_text("Quartos: 3")
+        expect(page).to have_text("Banheiros: 2")
+        expect(page).to have_text("Aceita pets: Sim")
+        expect(page).to have_text("Estacionamento: Sim")
     end
 end
